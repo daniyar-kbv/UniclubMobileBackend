fd324844889985fcfd384a4708ef5b0a59e1bfca
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-11-10 23:50:51 +0600

    deploy

--- uniclub_mobile/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uniclub_mobile.settings')
-
-app = Celery('uniclub_mobile')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- uniclub_mobile/settings.py
+++ uniclub_mobile/settings.py
@@ -128,10 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CELERY_BROKER_URL = os.environ.get('BROKER_URL', 'amqp://localhost')
-CELERY_IGNORE_RESULT = False
-CELERY_RESULT_BACKEND = 'amqp'
-
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = [
     'access-control-allow-origin',
