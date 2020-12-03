50debf19aa0646472fa613ffa78aa801d1c0c8a3
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-12-03 19:58:55 +0600

    telegrambot init

--- main/telegrambot.py
+++ main/telegrambot.py
@@ -3,9 +3,6 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from django_telegrambot.apps import DjangoTelegramBot
 
-import logging
-logger = logging.getLogger(__name__)
-
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
@@ -19,14 +16,13 @@ def echo(bot, update):
 
 def error(bot, update, error):
     print(error)
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
 def main():
-    logger.info("Loading handlers for telegram bot")
-
     dp = DjangoTelegramBot.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+
+    dp.add_handler(MessageHandler([Filters.text], echo))
 
     dp.add_error_handler(error)
