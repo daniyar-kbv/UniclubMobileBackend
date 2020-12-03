a8c947eefdea593cc08db8914215652fdee91448
daniyarka <daniyarkurmanbayev123@gmail.com>
2020-12-04 02:11:37 +0600

    telegrambot init

--- utils/telegrambot.py
+++ utils/telegrambot.py
@@ -6,6 +6,7 @@ bot.set_webhook(url=constants.TELEGRAM_BOT_URL)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    print(message.text)
     bot.reply_to(message,
                  ("Hi there, I am EchoBot.\n"
                   "I am here to echo your kind words back to you."))
