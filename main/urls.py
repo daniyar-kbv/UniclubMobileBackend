2fd3fb3af02be7b1733610b7d0adba1ab32e39e7
daniyarka <daniyar.kbv@gmail.com>
2021-01-21 00:36:43 +0600

    club id active added

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import CourseViewSet \
-    # , TelegramBotView
+    , TelegramBotView
 
 router = DefaultRouter()
 router.register('courses', CourseViewSet)
 
 urlpatterns = [
-    # path('bot/', TelegramBotView.as_view())
+    path('bot/', TelegramBotView.as_view())
 ]
 
 urlpatterns += router.urls
